eeae2e0d8a294774aedd04e628f19a5f565ab03e
Rahul <rahulkhatri137@gmail.com>
2023-10-13 03:00:13 +0000

    Revert "RVX: use `ReX` Patches"

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ if [ ! -f "$HTMLQ" ]; then
 	rm "${TEMP_DIR}/htmlq.tar.gz"
 fi
 
-log "**Changelog**: [ReVanced-Extended](https://github.com/YT-Advanced/ReX-patches/releases/) | [Revanced](https://github.com/revanced/revanced-patches/releases/)"
+log "**Changelog**: [ReVanced-Extended](https://github.com/inotia00/revanced-patches/releases/) | [Revanced](https://github.com/revanced/revanced-patches/releases/)"
 log "**Note**: _MicroG must be installed!_\n"
 
 set_prebuilts() {
